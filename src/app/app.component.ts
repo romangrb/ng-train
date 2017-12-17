0f190f936db92840c869e815359249daf7e7bbb6
Roman <grabarroman@gmail.com>
2017-12-17 21:50:16 +0000

    #2 task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { formsModule } from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  
+        username ='';
+        
+        isInputDisabled = true;
+        
+        checkValidate(){
+            this.isInputDisabled = (this.username=='') true : false;
+        }
+        
+        onSubmit(){
+            this.username = '';
+        }
+    }
+  
 }
