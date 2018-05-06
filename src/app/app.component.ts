85781f7c696827ab887c2407db0368a666ca3137
roman <grabarroman@gmail.com>
2018-05-06 16:28:58 +0300

    task initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    .white-class{color:white}
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    
-    is_psw_show: boolean = false;
-    
-    logs: Array<number> = [];
-    
-    private createTimestamp(){
-        return Date.now() / 1000 | 0;
-    }
-    
-    toggleSecret(){
-        this.logs.push(this.createTimestamp());
-        this.is_psw_show = !this.is_psw_show;
-    }
+
 }
