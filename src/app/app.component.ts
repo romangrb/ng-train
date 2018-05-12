236757e21f5ec5591c6abfe8fc10b8f3216d007f
roman <grabarroman@gmail.com>
2018-05-12 22:44:48 +0300

    service event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AccountsService } from './accounts.service';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent implements OnInit {
-  accounts: {name: string, status: string}[] = [];
-
-  constructor(private accountsService: AccountsService) {}
-
-  ngOnInit() {
-    this.accounts = this.accountsService.accounts;
-  }
+export class AppComponent {
 }
